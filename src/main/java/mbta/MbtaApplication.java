df4cc3cb0836eda96797564b25cd3bbfbea07237
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-09-01 15:09:03 -0400

    Initial commit

---
+++ src/main/java/mbta/MbtaApplication.java
@@ -0,0 +1,15 @@
+package mbta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtaApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MbtaApplication.class, args);
+    }
+
+}
