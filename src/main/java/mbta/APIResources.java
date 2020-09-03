0e1db0f50a2f692657a3e01d87afd1aa7af22232
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-09-03 09:45:41 -0400

    url

--- src/main/java/mbta/APIResources.java
+++ src/main/java/mbta/APIResources.java
@@ -3,7 +3,7 @@ package mbta;
 public class APIResources
 {
 
-    public static final String MBTA_URL = System.getenv("MBTA_URL");
+    public static final String MBTA_URL = "https://api-v3.mbta.com";
     public static final String MBTA_KEY = System.getenv("MBTA_KEY");
 
 }
